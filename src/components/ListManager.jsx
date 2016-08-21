86dba94cd3c1845fe849eead128219f19308969a
David Zielski <davidzielski@gmail.com>
2016-08-21 07:06:38 -0700

    added react functionality

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -22,14 +22,38 @@ var ListManager = React.createClass({
     },
 
     render: function() {
+
+        var divStyle = {
+            marginTop: 10
+        };
+
+        var headingStyle = {
+
+        };
+
+        // if the user passed in a heading color to use, add it
+        if (this.props.headingColor) {
+            headingStyle.background = this.props.headingColor;
+        };
+
         return (
-            <div>
-                <h3>{this.props.title}</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.onChange} value={this.state.newItemText} />
-                    <button>Add</button>
-                </form>
+            <div style={divStyle} className="col-sm-4">
+                <div className="panel panel-primary">
+                    <div style={headingStyle} className="panel-heading">
+                        <h3>{this.props.title}</h3>
+                    </div>
+                    <div className="row panel-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="col-sm-9">
+                                <input className="form-control" onChange={this.onChange} value={this.state.newItemText} />
+                            </div>
+                            <div className="col-sm-2">
+                                <button className="btn btn-primary">Add</button>
+                            </div>
+                        </form>
+                    </div>
                 <List items={this.state.items} />
+                </div>
             </div>
         );
     }
